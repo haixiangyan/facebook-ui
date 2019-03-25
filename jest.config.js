94c692f39748905fad1ade21a96e8b82add5f2d4
Haixiang <haixiang6123@gmail.com>
2019-03-25 13:26:53 -0700

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    verbose: true,
+    clearMocks: false,
+    collectCoverage: false,
+    reporters: ["default"],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    moduleDirectories: ['node_modules'],
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js"
+    },
+    testMatch: ['<rootDir>/**/__tests__/**/*.spec.(js|jsx|ts|tsx)'],
+    transform: {
+        "^.+unit\\.(js|jsx)$": "babel-jest",
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    setupFilesAfterEnv: ["<rootDir>test/setupTests.js"]
+}
---
+++ lib/__tests__/Button.spec.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import * as renderer from 'react-test-renderer'
+import Button from '../Button'
+
+describe('Button', () => {
+  it('should be a div', () => {
+    const json = renderer.create(<Button/>).toJSON()
+    expect(json).toMatchSnapshot()
+  })
+})
