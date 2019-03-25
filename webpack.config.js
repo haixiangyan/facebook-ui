745ee06d41f786a7571a3de77d7c5de3213111d8
Haixiang <haixiang6123@gmail.com>
2019-03-25 12:08:38 -0700

    Separate webpack config file to prod and dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-    mode: 'production',
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.jsx']
     },
@@ -20,25 +18,5 @@ module.exports = {
                 loader: 'awesome-typescript-loader'
             }
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'facebook-ui',
-            template: 'index.html'
-        })
-    ],
-    externals: {
-        react: {
-            commonjs: 'react',
-            commonjs2: 'react',
-            amd: 'react',
-            root: 'React'
-        },
-        'react-dom': {
-            commonjs: 'react-dom',
-            commonjs2: 'react-dom',
-            amd: 'react-dom',
-            root: 'ReactDOM'
-        }
     }
 }
