e36510c30c4ffbc7f18dc9c31d5be5fb668e3814
Haixiang <haixiang6123@gmail.com>
2019-03-25 23:14:23 -0700

    Add tsconfig.test.json

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
     reporters: ["default", "jest-junit"],
     globals: {
         'ts-jest': {
-            tsConfig: 'tsconfig.json',
+            tsConfig: 'tsconfig.test.json',
         },
     },
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
