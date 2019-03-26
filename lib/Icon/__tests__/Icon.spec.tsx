4a564229fb50cc8803775645b645549f73d1240c
Haixiang <haixiang6123@gmail.com>
2019-03-25 21:52:37 -0700

    Add even handler test case for Icon

--- lib/Icon/__tests__/Icon.spec.tsx
+++ lib/Icon/__tests__/Icon.spec.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react'
 import * as renderer from 'react-test-renderer'
+import { mount } from 'enzyme'
 import Icon from '../Icon'
 
 describe('Icon', () => {
-    it('exists', () => {
-        const json = renderer.create(<Icon name="trashcan"/>).toJSON()
-        expect(json).toMatchSnapshot()
-    })
+  it('can render', () => {
+    const json = renderer.create(<Icon name="video"/>).toJSON()
+    expect(json).toMatchSnapshot()
+  })
+  it('can handle event', () => {
+    const handler = jest.fn()
+    const component = mount(<Icon name="video" onClick={handler}/>)
+    component.find('svg').simulate('click')
+    expect(handler).toBeCalled()
+  })
 })
