5fc5410a52f008d20dbb3e4ab33eaee4df1f5813
Haixiang <haixiang6123@gmail.com>
2019-03-25 23:52:44 -0700

    Localize icons and publish v0.0.2

--- lib/Icon/Icon.tsx
+++ lib/Icon/Icon.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import classes from '../utils/classes'
-import './icons/index'
+import './icons.js'
 import './Icon.scss'
 
 interface IconProps extends React.SVGAttributes<SVGElement> {
@@ -10,7 +10,7 @@ interface IconProps extends React.SVGAttributes<SVGElement> {
 const Icon: React.FunctionComponent<IconProps> = ({className, name, ...restProps}) => {
   return (
     <svg className={classes('fb-icon', className)} {...restProps}>
-      <use xlinkHref={`#icon_${name}`}/>
+      <use xlinkHref={`#icon-icon_${name}`}/>
     </svg>
   )
 }
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,19 @@
 export { default as Icon } from './Icon/Icon'
+
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import Icon from './Icon/Icon'
+
+const fn: React.MouseEventHandler = (e: React.MouseEvent) => {
+  console.log(e.target)
+  console.log((e.target as SVGElement).classList)
+}
+
+ReactDOM.render(
+  <div>
+    <Icon className={'video'} name="trashcan"/>
+    <Icon name="skin"/>
+    <Icon name="sport" onMouseEnter={() => console.log(1)} onMouseDown={() => console.log(2)} onClick={fn}/>
+  </div>,
+  document.getElementById('root')
+)
