0f35a5010bc731d7ef354b80f8ba5c2e26a399c0
Haixiang <haixiang6123@gmail.com>
2019-03-26 10:52:19 -0700

    Update examples

--- examples/script.tsx
+++ examples/script.tsx
@@ -2,18 +2,10 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import FbIcon from '../lib/Icon/FbIcon'
 
-const fn: React.MouseEventHandler = (e: React.MouseEvent) => {
-  console.log(e.target)
-  console.log((e.target as SVGElement).classList)
-}
-
 ReactDOM.render(
-  <div>
-    <h1>Hello</h1>
-    <FbIcon className={'test-icon'} name="trashcan"/>
-    <FbIcon style={{width: '2em', height: '2em'}} name="skin"/>
-    <FbIcon name="sport" onMouseEnter={() => console.log(1)} onMouseDown={() => console.log(2)} onClick={fn}/>
-    <FbIcon spin={true} name="shakehands" solid={true}/>
-  </div>,
+    <div>
+        <FbIcon name="video"/>
+        <FbIcon name="sports"/>
+    </div>,
   document.getElementById('root')
 )
