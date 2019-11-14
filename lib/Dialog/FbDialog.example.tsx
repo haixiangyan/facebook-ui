c22d0b71fb7d7cab310299a1dff971d318aab78d
Haixiang <haixiang6123@gmail.com>
2019-11-14 14:09:41 -0800

    Update dialog props

--- lib/Dialog/FbDialog.example.tsx
+++ lib/Dialog/FbDialog.example.tsx
@@ -4,12 +4,35 @@ import {useState} from 'react'
 
 export default function () {
   const [x, setX] = useState(false)
+  const [y, setY] = useState(false)
   return (
     <div>
-      <button onClick={() => setX(!x)}>Click</button>
-      <FbDialog visible={x}>
-        <strong>hi</strong>
-      </FbDialog>
+      <div>
+        <h1>Example 1</h1>
+        <button onClick={() => setX(!x)}>Click</button>
+        <FbDialog closeOnClickMask={true} visible={x} buttons={
+          [
+            <button key={1} onClick={() => setX(false)}>1</button>,
+            <button key={2} onClick={() => setX(false)}>2</button>
+          ]
+        } onClose={() => setX(false)}>
+          <strong>hi</strong>
+        </FbDialog>
+      </div>
+      <div>
+        <div>
+          <h1>Example 2</h1>
+          <button onClick={() => setY(!y)}>Click</button>
+          <FbDialog closeOnClickMask={false} visible={y} buttons={
+            [
+              <button key={1} onClick={() => setY(false)}>1</button>,
+              <button key={2} onClick={() => setY(false)}>2</button>
+            ]
+          } onClose={() => setY(false)}>
+            <strong>hi</strong>
+          </FbDialog>
+        </div>
+      </div>
     </div>
   )
 }
--- lib/Dialog/FbDialog.tsx
+++ lib/Dialog/FbDialog.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {Fragment, ReactNode} from 'react'
+import {Fragment, ReactElement, ReactNode} from 'react'
 
 import './FbDialog.scss'
 import {FbIcon} from '../index'
@@ -8,30 +8,50 @@ import {createScopedClass} from '../utils/classes'
 interface Props {
   visible: boolean
   children: ReactNode
+  buttons: ReactElement[]
+  onClose: React.MouseEventHandler
+  closeOnClickMask?: boolean
 }
 
 const scopedClass = createScopedClass('fb-dialog')
 const sc = scopedClass
 
 const FbDialog: React.FunctionComponent<Props> = (props) => {
+  const onClickClose: React.MouseEventHandler = (e) => {
+    props.onClose(e)
+  }
+
+  const onClickMask: React.MouseEventHandler = (e) => {
+    if (props.closeOnClickMask) {
+      props.onClose(e)
+    }
+  }
+
   return (
     props.visible ? (
         <Fragment>
-          <div className={sc('mask')}/>
+          <div className={sc('mask')} onClick={onClickMask}/>
+
           <div className={sc()}>
-            <div className={sc('close')}>
+            <div className={sc('close')} onClick={onClickClose}>
               <FbIcon name={'close'}/>
             </div>
+
             <header className={sc('header')}>Confirm</header>
-            <main className={sc('main')}>{props.children}</main>
+            <main className={sc('main')}>
+              {props.children}
+            </main>
             <footer className={sc('footer')}>
-              <button>OK</button>
-              <button>Cancel</button>
+              {props.buttons}
             </footer>
           </div>
         </Fragment>
       ) : null
   )
+}
+
+FbDialog.defaultProps = {
+  closeOnClickMask: false
 }
 
 export default FbDialog
