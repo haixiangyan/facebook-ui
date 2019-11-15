b3dd9e788bc8fca20366b5d659f1f0e0450a7d41
Haixiang <haixiang6123@gmail.com>
2019-11-15 14:27:41 -0800

    Make sider horizontal

--- lib/Layout/FbLayout.tsx
+++ lib/Layout/FbLayout.tsx
@@ -1,17 +1,27 @@
 import * as React from 'react'
+import {ReactElement} from 'react'
 import {createScopedClass} from '../utils/classes'
-
+import FbSider from './FbSider'
 import './FbLayout.scss'
 
 const sc = createScopedClass('fb-layout')
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
+  children: ReactElement | ReactElement[]
 }
 
 const FbLayout: React.FunctionComponent<Props> = (props) => {
   const {className, ...restProps} = props
+  let hasSider = false
+  if ((props.children as ReactElement[]).length) {
+    (props.children as ReactElement[]).map(node => {
+      if (node.type === FbSider) {
+        hasSider = true
+      }
+    })
+  }
   return (
-    <div className={sc('', { extra: className })} {...restProps}>
+    <div className={sc('', { extra: [className, hasSider && 'has-sider'].join(' ') })} {...restProps}>
       {props.children}
     </div>
   )
