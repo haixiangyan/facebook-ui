532e31b0d7ee652f8848178950308b1f859607ea
Haixiang <haixiang6123@gmail.com>
2019-11-15 11:47:23 -0800

    Rename props interface

--- lib/Icon/FbIcon.tsx
+++ lib/Icon/FbIcon.tsx
@@ -3,13 +3,13 @@ import {classes} from '../utils/classes'
 import './icons/index'
 import './FbIcon.scss'
 
-interface IconProps extends React.SVGAttributes<SVGElement> {
+interface Props extends React.SVGAttributes<SVGElement> {
   name: string
   solid?: boolean
   spin?: boolean
 }
 
-const FbIcon: React.FunctionComponent<IconProps> =
+const FbIcon: React.FunctionComponent<Props> =
   ({className, name, solid, spin, ...restProps}) => {
     const link = `#icon_${name}` + (solid ? '_fill' : '')
     const classNames = classes('fb-icon', className, spin && 'fb-icon-spin')
