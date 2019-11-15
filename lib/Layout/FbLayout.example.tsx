3b8cd9564659ca03c065b1bbaea13ebf0c905672
Haixiang <haixiang6123@gmail.com>
2019-11-15 12:06:47 -0800

    Add more examples

--- lib/Layout/FbLayout.example.tsx
+++ lib/Layout/FbLayout.example.tsx
@@ -3,16 +3,50 @@ import FbLayout from './FbLayout'
 import FbHeader from './FbHeader'
 import FbContent from './FbContent'
 import FbFooter from './FbFooter'
+import FbSider from './FbSider'
 
 export default function() {
   return (
     <div>
-      <h1>Example 1</h1>
       <div>
+        <h1>Example 1</h1>
         <FbLayout className="my-layout" style={{height: 500}}>
           <FbHeader>My Header</FbHeader>
           <FbContent>My Content</FbContent>
           <FbFooter>My Footer</FbFooter>
+        </FbLayout>
+      </div>
+      <div>
+        <h1>Example 2</h1>
+        <FbLayout className="my-layout" style={{height: 500}}>
+          <FbHeader>My Header</FbHeader>
+          <FbLayout>
+            <FbSider>Sider</FbSider>
+            <FbContent>Content</FbContent>
+          </FbLayout>
+          <FbFooter>My Footer</FbFooter>
+        </FbLayout>
+      </div>
+      <div>
+        <h1>Example 3</h1>
+        <FbLayout className="my-layout" style={{height: 500}}>
+          <FbHeader>My Header</FbHeader>
+          <FbLayout>
+            <FbContent>Content</FbContent>
+            <FbSider>Sider</FbSider>
+          </FbLayout>
+          <FbFooter>My Footer</FbFooter>
+        </FbLayout>
+      </div>
+      <div>
+        <h1>Example 4</h1>
+        <FbLayout className="my-layout" style={{height: 500}}>
+          <FbSider>Sider</FbSider>
+          <FbLayout>
+            <FbHeader>My Header</FbHeader>
+            <FbContent>Content</FbContent>
+            <FbFooter>My Footer</FbFooter>
+          </FbLayout>
         </FbLayout>
       </div>
     </div>
--- lib/Layout/FbSider.tsx
+++ lib/Layout/FbSider.tsx
@@ -1,8 +1,19 @@
 import * as React from 'react'
+import {createScopedClass} from '../utils/classes'
 
-const FbSider: React.FunctionComponent = () => {
+import './FbLayout.scss'
+
+const sc = createScopedClass('fb-layout')
+
+interface Props extends React.HTMLAttributes<HTMLElement> {
+}
+
+const FbSider: React.FunctionComponent<Props> = (props) => {
+  const {className, ...restProps} = props
   return (
-    <div>x</div>
+    <div className={sc('sider', { extra: className })} {...restProps}>
+      {props.children}
+    </div>
   )
 }
 
