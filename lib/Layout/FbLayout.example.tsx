cafb0e275c60e6d55c31ba200ace81a8977bb9b0
Haixiang <haixiang6123@gmail.com>
2019-11-15 17:32:12 -0800

    Update example styles

--- lib/Layout/FbLayout.example.tsx
+++ lib/Layout/FbLayout.example.tsx
@@ -5,47 +5,49 @@ import FbContent from './FbContent'
 import FbFooter from './FbFooter'
 import FbSider from './FbSider'
 
+import './FbLayout.example.scss'
+
 export default function() {
   return (
     <div>
       <div>
         <h1>Example 1</h1>
         <FbLayout className="my-layout" style={{height: 500}}>
-          <FbHeader>My Header</FbHeader>
-          <FbContent>My Content</FbContent>
-          <FbFooter>My Footer</FbFooter>
+          <FbHeader className="header">My Header</FbHeader>
+          <FbContent className="content" style={{margin: '8px 0'}}>My Content</FbContent>
+          <FbFooter className="footer">My Footer</FbFooter>
         </FbLayout>
       </div>
       <div>
         <h1>Example 2</h1>
         <FbLayout className="my-layout" style={{height: 500}}>
-          <FbHeader>My Header</FbHeader>
+          <FbHeader className="header">My Header</FbHeader>
           <FbLayout>
-            <FbSider>Sider</FbSider>
-            <FbContent>Content</FbContent>
+            <FbSider className="sider" style={{margin: '8px 0'}}>Sider</FbSider>
+            <FbContent className="content" style={{margin: '8px 0 8px 8px'}}>Content</FbContent>
           </FbLayout>
-          <FbFooter>My Footer</FbFooter>
+          <FbFooter className="footer">My Footer</FbFooter>
         </FbLayout>
       </div>
       <div>
         <h1>Example 3</h1>
         <FbLayout className="my-layout" style={{height: 500}}>
-          <FbHeader>My Header</FbHeader>
+          <FbHeader className="header">My Header</FbHeader>
           <FbLayout>
-            <FbContent>Content</FbContent>
-            <FbSider>Sider</FbSider>
+            <FbContent className="content" style={{margin: '8px 0'}}>Content</FbContent>
+            <FbSider className="sider" style={{margin: '8px 0 8px 8px'}}>Sider</FbSider>
           </FbLayout>
-          <FbFooter>My Footer</FbFooter>
+          <FbFooter className="footer">My Footer</FbFooter>
         </FbLayout>
       </div>
       <div>
         <h1>Example 4</h1>
         <FbLayout className="my-layout" style={{height: 500}}>
-          <FbSider>Sider</FbSider>
+          <FbSider className="sider" style={{margin: '0 8px 0 0'}}>Sider</FbSider>
           <FbLayout>
-            <FbHeader>My Header</FbHeader>
-            <FbContent>Content</FbContent>
-            <FbFooter>My Footer</FbFooter>
+            <FbHeader className="header">My Header</FbHeader>
+            <FbContent className="content" style={{margin: '8px 0'}}>Content</FbContent>
+            <FbFooter className="footer">My Footer</FbFooter>
           </FbLayout>
         </FbLayout>
       </div>
