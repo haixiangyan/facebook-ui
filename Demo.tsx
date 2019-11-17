4842392b868bc9769078d7cc50130c96f085f8a4
Haixiang <haixiang6123@gmail.com>
2019-11-17 09:39:05 -0800

    Highlight codes

--- Demo.tsx
+++ Demo.tsx
@@ -1,16 +1,40 @@
 import * as React from 'react'
+import Highlight, {defaultProps} from 'prism-react-renderer'
+import {useState} from 'react'
 
 interface Props {
   code: string
 }
 
 const Demo: React.FunctionComponent<Props> = (props) => {
+  const [visible, setVisible] = useState(false)
+  const codes = (
+    <Highlight {...defaultProps} code={props.code.trim()} language="jsx">
+      {({className, style, tokens, getLineProps, getTokenProps}) => (
+        <pre className={className} style={style}>
+          {
+            tokens.map((line, i) => (
+              <div {...getLineProps({line, key: i})}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({token, key})}/>
+                ))}
+              </div>
+            ))
+          }
+          </pre>
+      )}
+    </Highlight>
+  )
+
   return (
     <div>
-      {props.children}
-      <pre>
-        {props.code}
-      </pre>
+      <div className="demo">
+        {props.children}
+      </div>
+      <div>
+        <button onClick={() => setVisible(!visible)}>Show Codes</button>
+        {visible && codes}
+      </div>
     </div>
   )
 }
